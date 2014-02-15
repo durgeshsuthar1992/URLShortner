b34c00e483690d4bb9358327d63eacaa4d21da71
Durgesh suthar <durgesh.30@rediffmail.com>
2014-02-15 21:34:27 +0530

    Initial commit with basic files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html5>
+<html>
+  <head lang="en">
+	<meta charset="utf-8">
+	<title>MyURLShortner</title>
+	<meta name="author" content="Durgesh">
+  </head>
+
+  <body>
+  	<form method="post" action="./">
+  		<label>Give your long URL and I will make it short for you</label>
+  		<input type="url" name="longURL" >
+  		<input type="submit" value="Short magic">
+  	</form>
+  	<div id="result">
+
+  	</div>
+  </body>
