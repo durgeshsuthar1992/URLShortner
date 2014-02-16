ca5c47893e103de4f3bb675670557712a4e824fc
Durgesh suthar <durgesh.30@rediffmail.com>
2014-02-16 16:34:02 +0530

    Error msg when short URL is not correct

--- redirect.php
+++ redirect.php
@@ -5,7 +5,10 @@ $ShortURL=HOST.$_GET['shorturl'];
 $sql=$db->prepare("SELECT long_url FROM urls WHERE short_url=?");
 $sql->execute(array($ShortURL));
 $LongURL=$sql->fetchColumn();
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: ' .  $LongURL);
-
+if($LongURL){
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: ' .  $LongURL);
+}else{
+	echo "This URL does not exist!!!";
+}
 ?>
